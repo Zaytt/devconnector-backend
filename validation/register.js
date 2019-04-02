1aade73f33562ce5d77beb1a25c1d410e4b8a3b6
Zaytt <ivan_chavez_e@hotmail.com>
2019-04-02 17:35:12 -0600

    Fixed typo

--- validation/register.js
+++ validation/register.js
@@ -14,21 +14,21 @@ module.exports = function validateRegisterInput(data) {
 
   if (Validator.isEmpty(data.name)) errors.name = "Name field is required";
 
+  if (!Validator.isEmail(data.email)) errors.email = "Email is invalid";
+
   if (Validator.isEmpty(data.email)) errors.email = "Email field is required";
 
-  if (!Validator.isEmail(data.email)) errors.email = "Email is invalid";
+  if (!Validator.equals(data.password, data.password2))
+    errors.password2 = "Passwords must match";
+
+  if (!Validator.isLength(data.password, { min: 6, max: 30 }))
+    errors.password = "Pasword must be between 6 and 30 characters";
 
   if (Validator.isEmpty(data.password))
     errors.password = "Password field is required";
 
-  if (!Validator.isLength(data.password, { min: 6, max: 30 }))
-    errors.pasword = "Pasword must be between 6 and 30 characters";
-
   if (Validator.isEmpty(data.password2))
     errors.password2 = "Confirm Password field is required";
-
-  if (!Validator.equals(data.password, data.password2))
-    errors.password = "Passwords must match";
 
   return {
     errors,
